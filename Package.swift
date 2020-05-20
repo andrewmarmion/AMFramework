cb08c2a9636e0c0bc05ec7b5fe66d3e378be766a
Andrew Marmion <andrew.marmion@gmail.com>
2020-05-20 23:25:15 +0100

    Added changes to Package.swift

--- Package.swift
+++ Package.swift
@@ -5,10 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "AMFramework",
+    platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "AMFramework",
+            type: .dynamic,
             targets: ["AMFramework"]),
     ],
     dependencies: [
